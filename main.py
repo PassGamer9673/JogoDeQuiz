b0e51c93b51ab35daa883996d3152c689c9e29ac
João Eurico Bokomoko Lima <34620239+Bokomoko@users.noreply.github.com>
2022-11-11 19:39:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+perguntas = ["Quem descobriu o Brasil?","Qual é a capital do Ceará?","Quanto é 3x71?"]
+respostas_corretas = ["Pedro Alvares Cabral","Fortaleza","203"]
+
+print (perguntas[0])
+resposta = input()
+print ("sua resposta foi "+resposta)
+if (resposta == respostas_corretas[0]):
+  print (" ")
+  print ("Resposta correta!")
+else:
+  print (" ")
+  print ("Resposta errada")
+
+print (perguntas[1])
+resposta = input()
+print ("sua resposta foi "+resposta)
+if (resposta == respostas_corretas[1]):
+  print (" ")
+  print ("Resposta correta!")
+else:
+  print (" ")
+  print ("Resposta errada")
+
+print (perguntas[2])
+resposta = input()
+print ("sua resposta foi "+resposta)
+if (resposta == respostas_corretas[2]):
+  print (" ")
+  print ("Resposta correta!")
+else:
+  print (" ")
+  print ("Resposta errada")
