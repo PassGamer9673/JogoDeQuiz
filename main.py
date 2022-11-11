19ea244b799402f84270a788fb25fe8f084684bd
PassGamer <118018552+PassGamer9673@users.noreply.github.com>
2022-11-11 21:08:36 +0000

    Adicionamos pontuação ao jogador, e mudamos algumas mensagens.

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 # inicialização do programa
-perguntas = ["Quem descobriu o Brasil?","Qual é a capital do Ceará?","Quanto é 3x71?", "Qual é o propósito do significado?"]
-respostas_corretas = ["Pedro Alvares Cabral","Fortaleza","203","Não sei"]
+perguntas = [ 
+   "Quem descobriu o Brasil?",
+   "Qual é a capital do Ceará?",
+   "Quanto é 3x71?", 
+   "Qual é o propósito do significado?",
+  "Quanto é 70+30?"
+]
+respostas_corretas = [
+  "Pedro Alvares Cabral",
+  "Fortaleza",
+  "203",
+  "Não sei",
+  "100"
+]
+pontuacao = [
+  5,
+  10,
+  15,
+  0,
+  5
+]
+pontos_do_jogador = 0
 # aqui termina a inicialização
 
 #começa o jogo para valer a partir daqui 
 # vamos usar um laço de repetição na lista de perguntas
-for i in range(4):
+quantasPerguntas = len(perguntas)
+for i in range(quantasPerguntas):
   print (perguntas[i])
   resposta = input()
   print ("sua resposta foi "+resposta)
   if (resposta == respostas_corretas[i] ):
     print (" ")
-    print ("Resposta correta!")
+    print (f"Resposta correta! Você ganhou {pontuacao[i]} pontos!")
     print (" ")
+    # acumulador
+    pontos_do_jogador += pontuacao[i]
   else:
     print (" ")
     print ("Resposta errada")
     print (" ")
+print(f"Acabou! Você fez {pontos_do_jogador}")
